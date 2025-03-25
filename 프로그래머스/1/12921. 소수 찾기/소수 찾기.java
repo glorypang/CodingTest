ccc69302c65f1538301715d8268d2b6995d19401
Glory <78744049+glorypang@users.noreply.github.com>
2025-03-25 19:42:52 +0900

    [level 1] Title: 소수 찾기, Time: 25.84 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,23 +1,32 @@
+import java.util.Arrays;
+
 class Solution {
-    public int solution(int n) {
+    public int solution(int n){
         int answer = 0;
-        for(int i = 1; i <= n; i++){
-            if (isPrime(i)){
-                answer++;
+        boolean[] arr1 = isPrime(n);
+        for(int i=1; i<=n; i++){
+            if(arr1[i] == true){
+                answer += 1;
             }
         }
         return answer;
     }
-
-    public static boolean isPrime(int n){
-        if(n < 2){
-            return false;
-        }
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if (n % i == 0) {
-                return false;
+    public static boolean[] isPrime(int n){
+        boolean[] arr = new boolean[n+1];
+        
+        Arrays.fill(arr,true);
+        arr[0]=false;
+        arr[1]=false;
+        
+        for(int i =2; i<=Math.sqrt(n); i++){
+            if(arr[i] == true){
+                for(int j=i*i;j<=n;j+=i){
+                    arr[j] = false;
+                }
             }
         }
-        return true;
-    }
+        return arr;
+        
+        
+     }
 }
