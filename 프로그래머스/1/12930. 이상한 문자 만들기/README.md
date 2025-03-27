82bc063e04bfd1751dea460f9001155714b6ecf2
Glory <78744049+glorypang@users.noreply.github.com>
2025-03-27 15:10:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.14 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
 
 ### 성능 요약
 
-메모리: 82 MB, 시간: 21.15 ms
+메모리: 70.8 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 26일 23:08:19
+2025년 03월 27일 15:10:55
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,31 +1,17 @@
 class Solution {
     public String solution(String s) {
-        int idx = 0;
-        String answer ="";
-        char c = ' ';
-        for(int i = 0 ; i < s.length() ; i++){
-            c = s.charAt(i);
-            if(s.charAt(i) == ' '){
-                idx = -1;
-                System.out.println("Enter");                
-            }
-            if(idx%2 == 0){
-                c = Character.toUpperCase(s.charAt(i));
-                answer = answer + c;
-
-            }
-            else{
-                c = Character.toLowerCase(s.charAt(i));
-                answer = answer + c;
-            }
-            System.out.println(c + " " + idx);                
-            
-            idx ++;
-
-                
-            
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
         
+        for(char c : s.toCharArray()){
+            if(c == ' '){
+                i = -1;
+            }
+            if(i%2 == 0 ) sb.append(Character.toUpperCase(c));
+            else sb.append(Character.toLowerCase(c));
+            
+            i++;
         }
-        return answer;
+        return sb.toString();
     }
 }
