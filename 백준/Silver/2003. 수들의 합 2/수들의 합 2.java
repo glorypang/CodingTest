fd76b50e2de37cf86ef08e159e156f6ac47239b7
Glory <78744049+glorypang@users.noreply.github.com>
2025-04-09 22:54:44 +0900

    [Silver IV] Title: 수들의 합 2, Time: 136 ms, Memory: 15740 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -3,44 +3,43 @@ import java.io.*;
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
+        String[]input = br.readLine().split(" ");
 
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
 
         input = br.readLine().split(" ");
-        int[] arr = new int[input.length + 1];
-        for (int i = 0; i < input.length; i++) {
+        int[] arr = new int[input.length+1];
+        for (int i = 0; i< input.length; i++) {
             arr[i] = Integer.parseInt(input[i]);
         }
+
+        int cnt = 0;
         int start = 0;
         int end = 0;
-        int cnt =0;
-        while((N) != start){
-            int sum = 0;
+        int sum = 0;
 
-            for(int i = start; i <= end; i++){
-                sum += arr[i];
-            }
-           // System.out.println(start + ", " + end + "=" + sum+ " " + cnt);
+        while(true){
 
-            if(end != N-1){
-                if(sum == M) {
-                    cnt++;
-                    end++;
-                }
-                else if(sum > M) start++;
-                else end++;
+            if(sum < M) {
+                if(end > N) break;
+                sum += arr[end++];
             }
-            else{
-                if(sum == M) {
-                    cnt++;
-                    start++;
-                }
-                else  start++;
-            }
+            else if(sum == M) {
+                cnt++;
+                sum -= arr[start++];
 
+            }
+            else {
+                sum -= arr[start++];
+            }
+            //System.out.println("after " + start + ", " + end + ", " + sum );
+            //System.out.println("----------------------------" );
+
+            //if(start == N-1 && start == end) break;
         }
+
         System.out.println(cnt);
     }
+
 }
