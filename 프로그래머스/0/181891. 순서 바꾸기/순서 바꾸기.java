de111e4ed07c799416eaf9bfdf0b6c5fbfd8f2ba
Glory <78744049+glorypang@users.noreply.github.com>
2025-04-22 12:55:32 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] arr1 = Arrays.copyOfRange(num_list, 0 , n);
+        int[] arr2 = Arrays.copyOfRange(num_list, n , num_list.length);
+        
+        int[] answer = new int[arr2.length+arr1.length];
+        System.arraycopy(arr2, 0, answer, 0, arr2.length);
+        System.arraycopy(arr1, 0, answer, arr2.length, arr1.length);
+        
+        
+        return answer;
+    }
+}
