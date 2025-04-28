e878b7c964cebde0a4873d0dfb9868e65143cfef
Glory <78744049+glorypang@users.noreply.github.com>
2025-04-28 22:06:20 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,9 +1,15 @@
 import java.util.*;
 class Solution {
-    public ArrayList<Integer> solution(int[] numbers, int num1, int num2) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = num1 ; i<= num2 ; i++)
+    public int[] solution(int[] numbers, int num1, int num2) {
+        List<Integer> arr = new ArrayList<>();
+        for(int i = num1 ; i <= num2 ; i++){
             arr.add(numbers[i]);
-        return arr;
+        }
+        int[] answer = new int[arr.size()];
+        for(int i = 0 ; i< arr.size();i++){
+            answer[i] = arr.get(i);
+        }
+        
+        return answer;
     }
 }
