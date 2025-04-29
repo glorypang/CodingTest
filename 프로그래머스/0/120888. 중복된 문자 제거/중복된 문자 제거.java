167dbc41caace72190a3353a70cb7ed355ed37a4
Glory <78744049+glorypang@users.noreply.github.com>
2025-04-29 14:08:30 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.16 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : my_string.toCharArray()){
+            if(!sb.toString().contains(String.valueOf(c)))
+                sb.append(c);
+        }
+        return sb.toString();
+    }
+}
