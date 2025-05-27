5ae70d01e87a962aea78a4b010ad4fe45ef58287
Glory <78744049+glorypang@users.noreply.github.com>
2025-05-27 19:16:28 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 816 ms, Memory: 196300 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,34 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n ;i++){
-            int num = Integer.parseInt(st.nextToken());
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < m ; i++){
-            int num = Integer.parseInt(st.nextToken());
-            if(map.containsKey(num)){
-                sb.append(map.get(num)).append(" ");
-            }
-            else{
-                sb.append(0).append(" ");
-            }
-        }
-        System.out.println(sb);
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		//문제에서 -10,000,000 ~ 10,000,000 , arr[index] = index - 100000000의 value를 가진 숫자가 몇 개인지 나타내주는 배열
+		int[] arr = new int[20000001];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			int input = Integer.parseInt(st.nextToken());
+			//해당 인덱스 값에 +1해준다.
+			arr[input + 10000000]++;
+		}
+		
+		int M = Integer.parseInt(br.readLine()); //찾고 싶은 숫자 개수 M
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			int input = Integer.parseInt(st.nextToken());
+			sb.append(arr[input + 10000000]).append(" ");
+		}
+		System.out.println(sb);
+	}
 }
