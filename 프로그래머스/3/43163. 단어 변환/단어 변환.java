103451965cda13b356897fecf5a48066665b28dc
Glory <78744049+glorypang@users.noreply.github.com>
2025-05-28 11:39:46 +0900

    [level 3] Title: 단어 변환, Time: 0.29 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,57 +1,45 @@
 import java.util.*;
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        
-        int correct = 0;
-        for(String str : words){
-            if(str.equals(target))
-                  correct++;
-        }
-        if(correct == 0) return 0;
-        
-        boolean[] visited = new boolean[words.length];
         Queue<String> q = new LinkedList<>();
-        q.add(begin);
-
-        visited[0] = true;
-        int depth = 0;
+        q.offer(begin);
+        boolean[] visit = new boolean[words.length+1];
         
-        while (!q.isEmpty()) {
-            int size = q.size(); // 현재 층의 노드 수
+        
+        int depth =0 ;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0 ; i< size; i++){                
+                String cur = q.poll();
 
-            for (int i = 0; i < size; i++) {
-                String word = q.poll();
-
-                if (word.equals(target)) {
+                if(cur.equals(target)){
                     return depth;
                 }
-
-                for (String next : words) {
-                    if (isOneLetterDifferent(word, next)) {
-                        q.add(next);
+                for(int j =0  ; j < words.length; j++){
+                    String next = words[j];
+                    if(isOneDifferen(cur, next) && !visit[j]){
+                        visit[j] = true;
+                        q.offer(next);
+                   
                     }
                 }
             }
-            depth++; // 층이 끝났으니 depth 증가
+            depth++;
         }
-
         
         return 0;
     }
-    public boolean isOneLetterDifferent(String a, String b) {
-    if (a.length() != b.length()) return false;
-
-    int diffCount = 0;
-    for (int i = 0; i < a.length(); i++) {
-        if (a.charAt(i) != b.charAt(i)) {
-            diffCount++;
+    public boolean isOneDifferen(String a, String b){
+        int cnt =0;
+        for(int i = 0 ; i< a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)){
+                cnt++;
+            }
+            if(cnt >= 2)
+                return false;
         }
-        if (diffCount > 1) {
-            return false;
-        }
+        return true;
     }
-    return diffCount == 1;
-}
-
 }
