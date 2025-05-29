8b166492a79338ca84b05c09c042f70f9a73dbc0
Glory <78744049+glorypang@users.noreply.github.com>
2025-05-29 20:59:33 +0900

    [level 3] Title: 단어 변환, Time: 0.21 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,43 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+        boolean[] visit = new boolean[words.length];
         Queue<String> q = new LinkedList<>();
         q.offer(begin);
-        boolean[] visit = new boolean[words.length+1];
+        int depth = 0;
         
-        
-        int depth =0 ;
         while(!q.isEmpty()){
             int size = q.size();
-            for(int i = 0 ; i< size; i++){                
-                String cur = q.poll();
-
-                if(cur.equals(target)){
+            
+            for(int i =0 ; i < size; i++){
+                String str = q.poll();
+                
+                if(str.equals(target)){
                     return depth;
                 }
-                for(int j =0  ; j < words.length; j++){
-                    String next = words[j];
-                    if(isOneDifferen(cur, next) && !visit[j]){
+                
+                for(int j = 0 ; j <words.length; j++){
+                    if(!visit[j] && isOneDifferent(str, words[j])){
                         visit[j] = true;
-                        q.offer(next);
-                   
+                        q.offer(words[j]);
                     }
                 }
             }
             depth++;
         }
-        
         return 0;
     }
-    public boolean isOneDifferen(String a, String b){
-        int cnt =0;
-        for(int i = 0 ; i< a.length(); i++){
-            if(a.charAt(i) != b.charAt(i)){
+    public boolean isOneDifferent(String s1, String s2){
+        int cnt = 0;
+        for(int i = 0 ; i <s1.length(); i++){
+            if(s1.charAt(i) != s2.charAt(i)){
                 cnt++;
             }
-            if(cnt >= 2)
+            if(cnt >= 2){
                 return false;
+            }
         }
         return true;
     }
