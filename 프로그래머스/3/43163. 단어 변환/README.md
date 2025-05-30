7ada04c5b9c481792e1c5386c92f21222bec0fff
Glory <78744049+glorypang@users.noreply.github.com>
2025-05-30 09:45:27 +0900

    [level 3] Title: 단어 변환, Time: 0.18 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 0.21 ms
+메모리: 72.7 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 29일 22:28:49
+2025년 05월 30일 09:45:26
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -6,19 +6,20 @@ class Solution {
         Queue<String> q = new LinkedList<>();
         q.offer(begin);
         int depth = 0;
-        
         while(!q.isEmpty()){
             int size = q.size();
             while(size-- > 0){
-                String cur = q.poll();
+                String cur =q.poll();
                 
                 if(cur.equals(target)){
                     return depth;
                 }
                 for(int i = 0 ; i< words.length; i++){
-                    if(!visit[i] && isOne(cur, words[i])){
+                    String next = words[i];
+
+                    if(!visit[i] && isOne(cur, next)){
                         visit[i] = true;
-                        q.offer(words[i]);
+                        q.offer(next);
                     }
                 }
             }
@@ -28,11 +29,11 @@ class Solution {
     }
     public boolean isOne(String s1, String s2){
         int cnt =0;
-        for(int i = 0 ; i < s1.length(); i++){
+        for(int i =0  ; i< s1.length(); i++){
             if(s1.charAt(i) != s2.charAt(i)){
                 cnt++;
             }
-            if(cnt>1){
+            if(cnt >1){
                 return false;
             }
         }
