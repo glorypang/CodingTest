7b5d3e18ed60006b2dadd151a48f9544c0b6ed08
Glory <78744049+glorypang@users.noreply.github.com>
2025-05-30 11:42:17 +0900

    [level 3] Title: 단어 변환, Time: 0.31 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -6,34 +6,36 @@ class Solution {
         Queue<String> q = new LinkedList<>();
         q.offer(begin);
         int depth = 0;
+        
         while(!q.isEmpty()){
             int size = q.size();
+            
             while(size-- > 0){
-                String cur =q.poll();
+                String cur = q.poll();
                 
                 if(cur.equals(target)){
                     return depth;
                 }
+            
                 for(int i = 0 ; i< words.length; i++){
-                    String next = words[i];
-
-                    if(!visit[i] && isOne(cur, next)){
+                    if(!visit[i] && isOne(cur, words[i])){
                         visit[i] = true;
-                        q.offer(next);
+                        q.offer(words[i]);
                     }
                 }
             }
             depth++;
         }
-        return answer;
+        return 0;
     }
+    
     public boolean isOne(String s1, String s2){
-        int cnt =0;
-        for(int i =0  ; i< s1.length(); i++){
+        int cnt = 0;
+        for(int i = 0 ; i < s1.length(); i++){
             if(s1.charAt(i) != s2.charAt(i)){
                 cnt++;
             }
-            if(cnt >1){
+            if(cnt>1){
                 return false;
             }
         }
