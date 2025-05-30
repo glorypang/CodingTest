4e63d3debf9ef8b6c4853d6b457079c6901d1c7e
Glory <78744049+glorypang@users.noreply.github.com>
2025-05-30 09:40:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.60 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,50 @@
 import java.util.*;
-
 class Solution {
-    static int[][] map;
-    static boolean[][] visited;
     static int[][] dist;
+    static boolean[][] visit;
+    static int[][] map;
     static int N, M;
     static int[] dx ={-1, 1, 0, 0};
-    static int[] dy ={0, 0, -1, 1};    
+    static int[] dy ={0, 0, -1, 1};
+    static int[] dx2 ={-2, 2, 0, 0};
+    static int[] dy2 ={0, 0, -2, 2};
     public int solution(int[][] maps) {
         N = maps.length;
         M = maps[0].length;
-        
         map = maps;
-        visited = new boolean[N][M];
         dist = new int[N][M];
-        return bfs(0,0);
+        visit = new boolean[N][M];
+        
+        
+        
+        return bfs(0, 0);
     }
     public int bfs(int x, int y){
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x,y});
-        visited[x][y] = true;
-        int cnt = 0;
+        q.offer(new int[]{x, y});
+        visit[x][y] = true;
+        
         while(!q.isEmpty()){
             int[] cur = q.poll();
             int cx = cur[0];
             int cy = cur[1];
-            if(cx == N-1 && cy == M-1){
-                        return dist[N-1][M-1]+1;
-
-            }
             
-            for(int i = 0 ; i< 4 ;i++){
+            if(cx == N-1 && cy == M-1){
+                return dist[cx][cy]+1;
+            }
+            for(int i = 0 ; i < 4 ;i++){
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
                 
-                 if(nx >= 0 && ny >= 0 && nx < N && ny < M){
-                     if(!visited[nx][ny] && map[nx][ny] == 1){
-                         q.offer(new int[]{nx, ny});
-                         visited[nx][ny] = true;
-                         dist[nx][ny] = dist[cx][cy] +1;
-                     }
-                 }
-
+                if(nx >= 0 && ny >= 0 && nx < N && ny < M){
+                    if(!visit[nx][ny] && map[nx][ny] == 1){
+                        visit[nx][ny]= true;
+                        q.offer(new int[]{nx, ny});
+                        dist[nx][ny] = dist[cx][cy]+1;
+                    }
+                }
             }
+            
         }
         return -1;
     }
