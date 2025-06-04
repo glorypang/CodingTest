d260aa1481d663fef9779f7dd1c805ea88d11fad
Glory <78744049+glorypang@users.noreply.github.com>
2025-06-04 14:49:00 +0900

    [Gold V] Title: 수 고르기, Time: 320 ms, Memory: 28084 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = 0;
+        int min = Integer.MAX_VALUE;
+
+        while (start < n && end < n) {
+            int diff = arr[end] - arr[start];
+
+            if (diff < m) {
+                end++;
+            } else {
+                min = Math.min(min, diff);
+                start++;
+            }
+        }
+
+        System.out.println(min);
+    }
+}
