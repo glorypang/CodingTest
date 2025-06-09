e7426871a9b28e96d3c8d0a3bfac2b1c9e1b2946
Glory <78744049+glorypang@users.noreply.github.com>
2025-06-09 22:26:15 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 6.43 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 가까운 같은 글자 - 142086 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086) 
 
 ### 성능 요약
 
-메모리: 98.1 MB, 시간: 70.66 ms
+메모리: 75.5 MB, 시간: 6.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 29일 12:34:59
+2025년 06월 09일 22:26:14
 
 ### 문제 설명
 
--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -2,16 +2,20 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
-        Arrays.fill(answer, -1);
-        
-        for(int i = s.length()-1 ; i >= 0 ; i--){ // 5 4 3 2 1 0
-            char charI = s.charAt(i);
-            for(int j = 0 ; j<i; j++){ // 0 1 2 3 4
-                char charJ = s.charAt(j);
-                if(charI == charJ) answer[i] = i-j;
+        Map<Character, Integer> map = new HashMap<>();
+        int idx = 0;
+        for(char c : s.toCharArray()){
+            if(!map.containsKey(c)){
+                map.put(c, idx);
+                answer[idx] = -1;
             }
+            else{
+                answer[idx] = idx - map.get(c);
+                map.put(c, idx);
+
+            }
+            idx++;
         }
-            
         return answer;
     }
 }
