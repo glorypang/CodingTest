2fd2d8c8195c70c2ea3ace1e0c88366be739122e
Glory <78744049+glorypang@users.noreply.github.com>
2025-06-09 18:25:25 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1096 ms, Memory: 76052 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -4,39 +4,43 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static List<List<Integer>> tree;
-    static Map<Integer, Integer> parent;
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-        int N = Integer.parseInt(br.readLine());
-        tree = new ArrayList<>();
-        for(int i = 0; i <= N; i++) {
-            tree.add(new ArrayList<>());
-        }
-        parent = new HashMap<>();
-        visited = new boolean[N+1];
+    static List<List<Integer>> g;
+    static boolean[] visit;
+    static int N;
+    static int[] res;
 
-        for(int i = 0 ; i < N-1 ; i++) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        visit = new boolean[N + 1];
+        res = new int[N + 1];
+        g = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            g.add(new ArrayList<>());
+        }
+        for (int i = 0; i < N-1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            tree.get(u).add(v);
-            tree.get(v).add(u);
+            g.get(u).add(v);
+            g.get(v).add(u);
         }
-
         dfs(1);
-        for(int i = 2 ; i <= N ; i++) {
-            System.out.println(parent.get(i));
+
+        for(int i = 2 ; i<= N ; i++){
+            System.out.println(res[i]);
         }
     }
-    public static void dfs(int start){
-        visited[start] = true;
 
-        for(int next: tree.get(start)) {
-            if(!visited[next]) {
-                parent.put(next, start);
+    public static void dfs(int n) {
+        visit[n] = true;
+        for (int next : g.get(n)) {
+            if (!visit[next]) {
+                visit[next] = true;
+                res[next] = n;
                 dfs(next);
+
             }
         }
     }
