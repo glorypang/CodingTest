245771dd09f52cebff67b7c20946b044ed7861ca
Glory <78744049+glorypang@users.noreply.github.com>
2025-06-23 19:35:22 +0900

    [level 0] Title: 옹알이 (1), Time: 3.87 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,22 +1,27 @@
 class Solution {
-    public int solution(String[] babbling) {
-        int answer = 0;
-        for(String str : babbling){
-            String temp = str.replace("aya", "");
-            temp = temp.replace("ye", "");
-            temp = temp.replace("woo", "");
-            temp = temp.replace("ma", "");
-            
-            System.out.println(temp);
-            
-            if(str.matches("^(aya|ye|woo|ma)+$")){
-                answer++;
-            }
-            
-            if(temp.length() == 0){
-                // answer++;
+    static String[] baby = {"aya", "ye", "woo", "ma"};
+
+    // 유효한 발음만으로 구성되었는지 확인
+    public static boolean isValid(String target) {
+        for (String word : baby) {
+            // 해당 발음을 반복적으로 제거
+            while (target.contains(word)) {
+                target = target.replace(word, " ");
             }
         }
+        System.out.println(target);
+        target = target.replace(" ", "");
+        // 다 제거되고 빈 문자열이면 true
+        return target.equals("");
+    }
+
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for (String word : babbling) {
+        if (isValid(word)) {
+                    answer++;
+                }
+            }
         return answer;
     }
 }
