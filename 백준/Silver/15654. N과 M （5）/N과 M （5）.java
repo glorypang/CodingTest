f2de0e925cfa9513c9dbd3e8351576e46631bfea
Glory <78744049+glorypang@users.noreply.github.com>
2025-07-01 16:54:13 +0900

    [Silver III] Title: N과 M (5), Time: 304 ms, Memory: 29492 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] visit;
+    static int[] res;
+    static int[] arr;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++ ) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        visit = new boolean[N];
+        res = new int[M];
+        dfs(N, M, 0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int N, int M, int dep) {
+        if (dep == M) {
+            for (int n : res) {
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                res[dep] = arr[i];
+                dfs(N, M, dep + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
