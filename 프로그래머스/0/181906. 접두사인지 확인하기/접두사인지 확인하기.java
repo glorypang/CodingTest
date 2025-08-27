e69bcdcd8687b4d96d609f0639722e022aba6f0f
Glory <78744049+glorypang@users.noreply.github.com>
2025-08-27 14:03:19 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        int answer = 0;
-        if(my_string.indexOf(is_prefix) == 0) return 1;
-        return 0;
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
