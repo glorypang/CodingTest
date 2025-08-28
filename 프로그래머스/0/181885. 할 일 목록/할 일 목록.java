746bf0b4a9a52a7ce1c7d11410a0fdce208a494f
Glory <78744049+glorypang@users.noreply.github.com>
2025-08-28 11:12:50 +0900

    [level 0] Title: 할 일 목록, Time: 0.32 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,18 +1,21 @@
+import java.util.*;
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        int cnt = 0;
-        for(boolean b : finished){
-            if(!b)
-                cnt++;
-        }
-        String[] answer = new String[cnt];
-        
+        String[] answer = {};
+        List<String> list = new ArrayList<>();
         
         int idx = 0;
-        for(int i = 0 ; i < todo_list.length; i++){
-            if(!finished[i])
-                answer[idx++] = todo_list[i];
+        for(String s : todo_list){
+            if(!finished[idx++]){
+                list.add(s);
+            }
         }
+        int len = list.size();
+        answer = new String[len];
+        for(int i = 0 ; i< len ; i++){
+            answer[i] = list.get(i);
+        }
+        System.out.println(list);
         return answer;
     }
 }
