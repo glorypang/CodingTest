dbfab528871fdfa06ac3c07d2efdc0a221502861
Glory <78744049+glorypang@users.noreply.github.com>
2025-08-28 20:42:10 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,10 +1,13 @@
 class Solution {
     public String solution(int age) {
-        StringBuilder sb = new StringBuilder();
-        String num = Integer.toString(age);
-        for(char c : num.toCharArray()){
-            sb.append((char)('a'+(c-'0')));
+        String answer = "";
+        StringBuilder sb =new StringBuilder();
+        while(age != 0){
+            int n = age%10;
+            age /= 10;
+            
+            sb.append((char)(97+n));
         }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
