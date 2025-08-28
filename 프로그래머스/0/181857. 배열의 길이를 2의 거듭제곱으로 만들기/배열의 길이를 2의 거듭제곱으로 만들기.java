4372baf7a1964d3714c0fe3d3601df649b43cadd
Glory <78744049+glorypang@users.noreply.github.com>
2025-08-28 22:19:22 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.06 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -1,28 +1,17 @@
+import java.lang.*;
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = {};
-        System.out.println(Math.sqrt(arr.length));
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int n  : arr)
-            list.add(n);
-        
-        while(!powerOfTwo(list.size())){
-            list.add(0);    
+        List<Integer> list = new ArrayList<>();
+        int n =(int)(Math.log10(arr.length)/Math.log10(2));
+        if(Math.log10(arr.length)/Math.log10(2)%1 != 0){
+            n++;
         }
-        int len = list.size();
-        answer = new int[len];
-        for(int i =0  ; i< len ; i++){
-            answer[i] = list.get(i);
+        n = (int)Math.pow(2,n);
+        int[] answer = new int[n];
+        for(int i = 0 ; i< arr.length; i++){
+            answer[i] = arr[i];
         }
         return answer;
     }
-    public boolean powerOfTwo(long num) {
-    while(num > 2){	
-        if(num % 2 == 1) 
-            return false;	
-        num /= 2;			
-    }
-    return true;
-}
 }
