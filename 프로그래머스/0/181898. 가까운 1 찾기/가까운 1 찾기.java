ff5c2c7358a227a596f2909e0645698dca19dad6
Glory <78744049+glorypang@users.noreply.github.com>
2025-08-28 11:00:06 +0900

    [level 0] Title: 가까운 1 찾기, Time: 1.46 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,11 +1,14 @@
 class Solution {
     public int solution(int[] arr, int idx) {
         int answer = 0;
-        for(int i = idx ; i < arr.length ; i++){
-            if(arr[i] == 1){
-                return i;
+        boolean check = false;
+        for(int i = idx ; i < arr.length; i++){
+            if(arr[i] == 1 && !check) {
+                check = true;
+                answer = i;
             }
         }
+        if(check) return answer;
         return -1;
     }
 }
