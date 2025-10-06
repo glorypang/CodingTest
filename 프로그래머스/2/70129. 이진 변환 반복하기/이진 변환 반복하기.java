37e4eafc6cbb7c3a8f7e7f6974dfb7a037e7514e
Glory <78744049+glorypang@users.noreply.github.com>
2025-10-06 09:50:45 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 24.67 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,26 +1,33 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = {};
-        
-        int idx = 0;
+
+        int sum = 0;        
         int cnt = 0;
-        while(!s.equals("1")){
-            String temp = s.replaceAll("0", "");;
-            cnt += s.length() - temp.length();
-            s= to2(temp.length());
-            idx++;
+
+        int after = 0;
+        while(after != 1){
+            int before = s.length();
+            after = s.replaceAll("0", "").length();
+
+            sum += before-after;
+            s = to2(after);
+            cnt++;
+
         }
-        return new int[]{idx, cnt};
+   
+
+        return new int[]{cnt, sum};
     }
-    public String to2(int n){
-        if(n==0){
-            return "0";
-        }
+    
+    public String to2 (int num){
+        
         StringBuilder sb = new StringBuilder();
-        while(n != 0){
-            sb.append(n%2);
-            n /= 2;
+        while(num != 0){
+            sb.append(num%2);
+            num /= 2;
         }
+        
         return sb.reverse().toString();
     }
 }
