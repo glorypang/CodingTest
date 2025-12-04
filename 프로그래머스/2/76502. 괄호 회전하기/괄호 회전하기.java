092851e583a1b1c12dc345e17e4d9f8d61ed1b9e
Glory <78744049+glorypang@users.noreply.github.com>
2025-12-04 10:33:18 +0900

    [level 2] Title: 괄호 회전하기, Time: 169.30 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,35 +2,42 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int len = s.length();
-        String temp =s;
         int answer = 0;
+        Deque<Character> str = new LinkedList<>();
+        for(Character a : s.toCharArray()){
+            str.addLast(a);
+        }
         
-        while(len-- > 0){
+        for(int i = 0; i < s.length(); i++){
             Stack<Character> st = new Stack<>();
-            for(char c: temp.toCharArray()){
+            StringBuilder sb = new StringBuilder();
+            for(Character c : str){
+                sb.append(c);
+            }
+            // System.out.println(sb);
+                
+            
+            for(int j = 0; j < s.length(); j++){
                 if(st.isEmpty()){
-                    st.push(c);
+                    st.push(sb.charAt(j));
                 }
                 else{
-                    if(st.peek() == '(' && c == ')'){
-                        st.pop();
-                    }
-                    else if(st.peek() == '[' && c == ']'){
-                        st.pop();
-                    }
-                    else if(st.peek() == '{' && c == '}'){
-                        st.pop();
-                    }
+                    if(st.peek() == '(' && sb.charAt(j) == ')') st.pop();
+                    else if(st.peek() == '[' && sb.charAt(j) == ']') st.pop();
+                    else if(st.peek() == '{' && sb.charAt(j) == '}') st.pop();
                     else {
-                        st.push(c);
+                        st.push(sb.charAt(j));
                     }
                 }
             }
-            if(st.isEmpty()){
-                answer++;
-            }
-            temp = temp.substring(1) + temp.charAt(0);
+            
+            // for(Character c : st){
+            //     System.out.println(c);
+            // }
+            if(st.isEmpty()) answer++;
+            
+            str.addLast(str.removeFirst());
+
         }
         return answer;
     }
