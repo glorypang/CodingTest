59cd01649acc73a29d49f817532921014400df60
Glory <78744049+glorypang@users.noreply.github.com>
2025-12-04 09:54:30 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 132.96 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,7 +3,6 @@ class Solution {
     public int solution(int[] elements) {
         int answer = 0;
         HashSet<Integer> set = new HashSet<>();
-        Deque<Integer> dq = new LinkedList<>();
         ArrayList<Integer> list = new ArrayList<>();
         
         int sum = 0;
@@ -18,28 +17,23 @@ class Solution {
             list.add(elements[i]);
         }
 
-
         
         for(int i = 2; i < elements.length; i++){ // 카운트 개수
             int std = 0;
             int z; int j;
-            for(j = 0 ; j < i; j++){ // 0 1 
-                std += elements[j]; // 5 j =2
+            for(j = 0 ; j < i; j++){ 
+                std += elements[j]; 
             }
             set.add(std);
-            //System.out.println(std);
-
-            z = j; //2
+            z = j; 
             
             for( ; z < list.size(); z++){
                 if(z-i == elements.length -1) break;
                 std += list.get(z);
                 std -= list.get(z-i);
-                //System.out.println(list.get(z-i) +", " +list.get(z) + ", " + std);
                 set.add(std);
             }
         }
-        
         return set.size();
     }
 }
