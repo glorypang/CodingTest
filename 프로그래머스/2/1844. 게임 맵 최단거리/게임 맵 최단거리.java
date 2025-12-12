513e3d90b443c3abd2a64f61b3e9cc1006b03002
Glory <78744049+glorypang@users.noreply.github.com>
2025-12-12 10:32:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.06 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,61 @@
 import java.util.*;
+
 class Solution {
-    static int[][] dist;
-    static boolean[][] visit;
-    static int[][] map;
-    static int N, M;
-    static int[] dx ={-1, 1, 0, 0};
-    static int[] dy ={0, 0, -1, 1};
-    static int[] dx2 ={-2, 2, 0, 0};
-    static int[] dy2 ={0, 0, -2, 2};
+    int r; int c;
+    boolean[][] visit;
+    int[][] dir = new int[][]{{-1,0}, {1,0}, {0,1},{0,-1}};
+    int[][] map;
+    int[][] dis;
+    
     public int solution(int[][] maps) {
-        N = maps.length;
-        M = maps[0].length;
         map = maps;
-        dist = new int[N][M];
-        visit = new boolean[N][M];
-        
-        
-        
-        return bfs(0, 0);
+        r = maps.length;
+        c = maps[0].length;
+        visit = new boolean[r][c];
+        dis = new int[r][c];
+        dis[0][0] =1;
+        bfs(0, 0);
+        // r = 1 , c = 5;
+        return dis[r-1][c-1] == 0 ? -1 : dis[r-1][c-1];
     }
-    public int bfs(int x, int y){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x, y});
+    
+    void bfs(int x, int y){
+        Queue<int[]> q = new LinkedList<>(); 
+        q.add(new int[]{x, y});
         visit[x][y] = true;
         
         while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
+            int[] pos = q.poll();
+            int cx = pos[0];
+            int cy = pos[1];
             
-            if(cx == N-1 && cy == M-1){
-                return dist[cx][cy]+1;
+            if(cx == r-1 && cy == c-1){
+                return ;
             }
-            for(int i = 0 ; i < 4 ;i++){
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
+            
+            for(int i = 0 ; i < 4; i++){
+                int nx = cx + dir[i][0]; // 1
+                int ny = cy + dir[i][1]; // 0
                 
-                if(nx >= 0 && ny >= 0 && nx < N && ny < M){
+                if(nx >= 0 && ny >= 0 && nx < r && ny < c){     
                     if(!visit[nx][ny] && map[nx][ny] == 1){
-                        visit[nx][ny]= true;
-                        q.offer(new int[]{nx, ny});
-                        dist[nx][ny] = dist[cx][cy]+1;
+                        q.add(new int[]{nx, ny});
+                        dis[nx][ny] = dis[cx][cy] + 1;
+                        visit[nx][ny] = true;
+                        
+                        // System.out.println(nx + ", " + ny);
+                        // for(int j= 0; j < r ; j++){
+                        //     for(int z = 0; z < c; z++){
+                        //         System.out.print(dis[j][z] + " ");
+                        //     }
+                        //     System.out.println();
+                        // }
+                        //                             System.out.println();
+
                     }
                 }
             }
-            
         }
-        return -1;
+        return ;
     }
 }
