b27cc184405e3fbfef1ad810434ffa70afdca894
Glory <78744049+glorypang@users.noreply.github.com>
2025-12-19 09:55:37 +0900

    [level 2] Title: 주식가격, Time: 53.31 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,15 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        for(int i = 0 ; i < prices.length; i++){
-            int time = 0;
-            for(int j = i+1; j < prices.length ; j++){
-                time++;
-                if(prices[i] > prices[j]) 
+        int len = prices.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0 ; i < len; i++){
+            int cnt = 0;
+            int j;
+            for(j = i+1; j <len; j++){
+                if(prices[i] <= prices[j]){
+                    cnt++;
+                }
+                else{
+                    cnt++;
                     break;
+                }
             }
-            answer[i] = time;            
+            map.put(i, cnt);
+            
         }
+        int[] answer = new int[map.size()];
+        for(int i = 0 ; i < len ;i++){
+            answer[i] = map.get(i);
+        }
+        
         return answer;
     }
 }
