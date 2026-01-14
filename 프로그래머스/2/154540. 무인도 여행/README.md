af0b67158c127ff850ab1e69338e6bcf4442d351
Glory <78744049+glorypang@users.noreply.github.com>
2026-01-14 10:32:15 +0900

    [level 2] Title: 무인도 여행, Time: 13.56 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/README.md
+++ 프로그래머스/2/154540. 무인도 여행/README.md
@@ -1,10 +1,10 @@
 # [level 2] 무인도 여행 - 154540 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154540#qna) 
 
 ### 성능 요약
 
-메모리: 92.8 MB, 시간: 2.71 ms
+메모리: 98 MB, 시간: 13.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 23일 18:44:44
+2026년 01월 14일 10:32:14
 
 ### 문제 설명
 
--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,58 +1,91 @@
 import java.util.*;
 class Solution {
-    int[] dx = {-1,1,0,0};
-    int[] dy = {0,0,-1,1};
-    boolean[][] visited;
-    int n, m;
+    int r; int c;
     char[][] map;
+    boolean[][] visit;
+    int[] dx = {0, 0 ,-1, 1};
+    int[] dy = {-1, 1, 0, 0};
     
+    int sum ;
     public int[] solution(String[] maps) {
-        n = maps.length;
-        m = maps[0].length();
-        visited = new boolean[n][m];
-        map = new char[n][m];
+        r = maps.length; c = maps[0].length();
+        map = new char[r][c];
+        visit = new boolean[r][c];
         
-        for(int i = 0 ; i< n ;i++){
-            map[i] = maps[i].toCharArray();
+        for(int i = 0 ;i < maps.length; i++){
+            for(int j = 0 ; j < maps[i].toCharArray().length; j++){
+                map[i][j] = maps[i].charAt(j);
+            }
         }
-    
-        List<Integer> dayList = new ArrayList<>();
         
-        for(int i = 0; i <n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(!visited[i][j] && map[i][j] != 'X'){
-                    int sum = dfs(i,j);
-                    dayList.add(sum);
+        // for(int a = 0 ; a < r; a++){
+        //                 for(int b = 0 ; b < c; b++){
+        //                     System.out.print((visit[a][b] ? "T" : "F") + " ");
+        //                 }
+        //                 System.out.println();
+        //             }
+        //                 System.out.println();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < r; i++){
+            for(int j = 0 ; j < c; j++){
+                if(map[i][j] != 'X' && !visit[i][j]){
+                    sum = 0;
+                    bfs(i, j);
+                    list.add(sum);
+                        
+                    // for(int a = 0 ; a < r; a++){
+                    //     for(int b = 0 ; b < c; b++){
+                    //         System.out.print((visit[a][b] ? "T" : "F") + " ");
+                    //     }
+                    //     System.out.println();
+                    // }
+                    //     System.out.println();
                 }
             }
         }
         
-        if(dayList.isEmpty()){
-            return new int[]{-1};
-        }
-        int[] answer = new int[dayList.size()];
-        for(int i = 0 ; i< dayList.size(); i++){
-            answer[i] = dayList.get(i);
-        }
-        Arrays.sort(answer);
+        if(list.size() == 0) return new int[]{-1};
         
+        Collections.sort(list);
+        int[] answer = new int[list.size()];
+        int idx =0;
+        for(int i : list){
+            answer[idx++] = i;
+        }
+        
+        
+        // for(int i = 0 ; i < r; i++){
+        //     for(int j = 0 ; j < c; j++){
+        //         System.out.print(map[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
         return answer;
     }
     
-    public int dfs(int x, int y){
-        visited[x][y] = true;
-        int sum = map[x][y] - '0';
-        
-        for(int dir = 0 ; dir < 4 ;dir++){
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+    void bfs(int x, int y){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        visit[x][y] = true;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
             
-            if(nx >= 0 && nx < n && ny >= 0 && ny < m){
-                if(!visited[nx][ny] && map[nx][ny] != 'X'){
-                    sum += dfs(nx,ny);
+            sum += map[cx][cy] - '0';
+            
+            for(int i= 0 ; i < 4 ;i++){
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                
+                if(nx < r && ny < c&& nx >=0 && ny >= 0 && map[nx][ny] != 'X'){
+                    if(!visit[nx][ny]){
+                        visit[nx][ny] = true;
+                        q.offer(new int[]{nx,ny});
+                    }
                 }
             }
+            
         }
-        return sum;
     }
 }
