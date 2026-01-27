feb5080a077b00a0e90d1edbee1ea1be8b464a16
Glory <78744049+glorypang@users.noreply.github.com>
2026-01-27 10:55:33 +0900

    [level 3] Title: 네트워크, Time: 2.17 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 0.68 ms
+메모리: 93.2 MB, 시간: 2.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 14:43:31
+2026년 01월 27일 10:55:33
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,43 +1,63 @@
 import java.util.*;
 class Solution {
-    static ArrayList<Integer>[] graph;
-    static boolean[] visited;
-    int n,m;
-    
+    int r; int c;
+    List<List<Integer>> graph = new ArrayList<>();
+    boolean[][] visit;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        m = computers[0].length;
+        r = computers.length; c = computers[0].length;
+        for(int i = 0 ; i < r+1; i++)
+            graph.add(new ArrayList<>());
+        visit = new boolean[r+1][c+1];
         
-        graph = new ArrayList[n+1];
-        visited = new boolean[n+1];
-        
-        
-        for(int i = 0; i < n ; i++){
-            graph[i+1] = new ArrayList<>();
-        }
-        
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(computers[i][j] == 1 && i != j){
-                    graph[i+1].add(j+1);
-                    graph[j+1].add(i+1);
+        for(int i = 0 ; i < r; i++){
+            for(int j = 0 ; j < c; j++){
+                if(computers[i][j] != 0){
+                    graph.get(i+1).add(j+1);
+                    
                 }
             }
         }
-        for(int i = 1 ; i<=n ; i++){
-            if(!visited[i]){
-                dfs(i);
-                answer++;
+        
+        // for(int i = 0 ; i < r+1; i++){
+        //     System.out.print(i + "번 노드 : ");
+        //     for(int j = 0 ; j < graph.get(i).size(); j++){
+        //         System.out.print(graph.get(i).get(j));
+        //     }
+        //     System.out.println();
+        // }
+        
+        int cnt = 0;
+        for(int i = 1 ; i <= r; i++){
+            for(int j = 0 ; j < graph.get(i).size(); j++){
+                if(!visit[i][j] && graph.get(i).size() >= 1){
+                    bfs(i, j);
+                    // System.out.println(i + " " + j);
+                    cnt++;
+                }
             }
         }
-        return answer;
+        
+        return cnt;
     }
-    public void dfs(int node){
-        visited[node] = true;
-        for(int next : graph[node]){
-            if(!visited[next]){
-                dfs(next);
+    
+    void bfs(int x, int y){ 
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        
+        while(!q.isEmpty()){ 
+            int[] cur = q.poll();
+            int cx = cur[0]; 
+            int cy = cur[1]; 
+            int num  = graph.get(cx).get(cy); 
+
+            for(int i = 0; i < graph.get(num).size(); i++){ 
+                if(!visit[num][i]){
+                    visit[num][i] = true;
+                    // System.out.println(num + " "  + i + " = "+ graph.get(num).get(i));
+                    q.offer(new int[]{num, i}); 
+                }
             }
+            
         }
     }
 }
