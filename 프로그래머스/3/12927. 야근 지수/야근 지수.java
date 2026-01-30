5ebad7bf7f0f0dd7f48add9e55e3b7c3b8745b2f
Glory <78744049+glorypang@users.noreply.github.com>
2026-01-30 09:46:29 +0900

    [level 3] Title: 야근 지수, Time: 135.74 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,18 +2,26 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int m : works)
-            pq.offer(m);
-        for(int i = 0 ; i < n ; i++){
-            int work = pq.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for(int w : works){
+            pq.offer(w);
+        }
+        
+        for(int i = 0 ; i < n ;i++){
+            if(pq.peek() == 0) return 0;
+            pq.offer(pq.peek()-1);
+            pq.poll();
             
-            if(work == 0) return 0;
-            pq.add(work -1);
+            // for(int p : pq){
+            //     System.out.print(p + " ");
+            // }
+            // System.out.println();
         }
-        for(int m : pq){
-            answer += Math.pow(m,2);
-        }
+            for(int p : pq){
+                answer += Math.pow(p,2);
+            }
+        
+        
         return answer;
     }
 }
