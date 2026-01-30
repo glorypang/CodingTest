21a63673606ba12a249e46d0fa463d79fdb0e21e
Glory <78744049+glorypang@users.noreply.github.com>
2026-01-30 13:11:19 +0900

    [level 3] Title: 등굣길, Time: 0.92 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
 
 ### 성능 요약
 
-메모리: 53.7 MB, 시간: 0.64 ms
+메모리: 71.9 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 16일 17:16:14
+2026년 01월 30일 13:11:18
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,20 +1,43 @@
 import java.util.*;
+
 class Solution {
-    public int solution(int m, int n, int[][] puddles) {
-        int[][] map = new int[n][m];
-        int[][] dp = new int[n+1][m+1];
+    int r; int c;
+    int[][] map;
+    public int solution(int n, int m, int[][] puddles) {
+        int answer = 0;
+        r = m; c = n;
+        map = new int[r][c];
         
-        for(int[] p : puddles){
-            map[p[1]-1][p[0]-1] = 1;
+        for(int i = 0; i < r; i++){
+            Arrays.fill(map[i],  1);
         }
-        dp[1][1] = 1; // 시작 위치
-        for(int i =0 ; i <n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(map[i][j] != 1){
-                    dp[i+1][j+1] += (dp[i][j+1] + dp[i+1][j])%1000000007;
+        for(int[] p : puddles){
+                map[p[1]-1][p[0]-1] = 0;
+        }
+        
+        
+        for(int i = 0 ; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(map[i][j] != 0){
+                    if(i == 0 && j >= 1){
+                        map[i][j] = map[i][j-1];
+                    }
+                    else if(j == 0 && i >=1){
+                        map[i][j] = map[i-1][j];
+                    }
+                    else if(i !=0 && j !=0 )
+                        map[i][j] = map[i-1][j]%1000000007 + map[i][j-1]%1000000007;  
                 }
             }
         }
-        return dp[n][m];
+        
+        // for(int i = 0 ; i < r; i++){
+        //     for(int j = 0; j < c; j++){
+        //         System.out.print(map[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        return map[r-1][c-1]%1000000007;
     }
 }
