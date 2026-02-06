60a1a67338dfb58f792268eb8a18e03ad83543ca
Glory <78744049+glorypang@users.noreply.github.com>
2026-02-06 17:26:41 +0900

    [level 3] Title: 스티커 모으기(2), Time: 13.27 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int solution(int sticker[]) {
+        int answer = 0;
+        int len = sticker.length;
+        
+        // 1번부터 시작
+        if(len == 1) return sticker[0];
+        else if(len == 2) return Math.max(sticker[0], sticker[1]);
+        else if(len == 3) return Math.max(sticker[0] + sticker[2] , sticker[1]);
+
+        int[] dp1 = new int[len];
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[1];
+        for(int i = 2; i < len ; i++){
+            if(i == len -1) continue;
+            if(i == 2){
+               dp1[i] = Math.max(dp1[0] + sticker[2] , sticker[1]);
+            }
+            else{
+                dp1[i] = Math.max(dp1[i-1], Math.max(dp1[i-2]+ sticker[i], dp1[i-3]+ sticker[i]));
+            }
+        }
+        
+        int[] dp2 = new int[len];
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        for(int i = 2; i < len ; i++){
+            if(i == 2){
+               dp2[i] = Math.max(dp2[0] + sticker[2] , sticker[1]);
+            }
+            else{
+                dp2[i] = Math.max(dp2[i-1], Math.max(dp2[i-2]+ sticker[i], dp2[i-3]+ sticker[i]));
+            }
+        }
+        
+        return Math.max(dp1[len-2], dp2[len-1]);
+    }
+}
