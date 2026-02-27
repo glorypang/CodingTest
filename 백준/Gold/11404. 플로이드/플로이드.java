18709b8adae46aacca1ca8ba55517e22ee9625b1
Glory <78744049+glorypang@users.noreply.github.com>
2026-02-27 14:09:38 +0900

    [Gold IV] Title: 플로이드, Time: 492 ms, Memory: 43892 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,68 +1,42 @@
-import org.w3c.dom.Node;
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static List<List<int[]>> graph;
-    static final int INF = Integer.MAX_VALUE;
-    static int[] dist;    // 거리 배열
-
+public class Main{
+    public static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        int V = Integer.parseInt(br.readLine());
-        int E = Integer.parseInt(br.readLine());
-//        int K = Integer.parseInt(br.readLine());
-
-
-        graph = new ArrayList<>();
-        for(int i = 0; i <= V; i++){
-            graph.add(new ArrayList<>());
+        int[][] map = new int[N+1][N+1];
+        for(int i = 1; i <= N; i++){
+            Arrays.fill(map[i], INF);
         }
 
-        for (int i = 0; i < E; i++) {
+        for(int i =0; i < M; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-
-            graph.get(s).add(new int[]{e, w});
+            map[u][v] = Math.min(map[u][v], w);
         }
 
-        for(int i = 1 ; i <= V ; i++){
-            dist = new int[V + 1];
-            Arrays.fill(dist, INF);
-
-            dijkstra(i);
-            for(int j = 1; j <= V; j++){
-                System.out.print(dist[j] == INF ? 0 +" " : dist[j]+" ");
-            }
-            System.out.println();
-        }
-    }
-    public static void dijkstra(int start) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        dist[start] = 0;
-        pq.offer(new int[]{start, 0});
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curNode = cur[0];
-            int curDist = cur[1];
-
-            if(curDist != dist[curNode]) continue;
-
-            for(int[] next : graph.get(curNode)) {
-                int nextNode = next[0];
-                int weight = next[1];
-
-                int newDist = curDist + weight;
-                if(dist[nextNode] > newDist) {
-                    dist[nextNode] = newDist;
-                    pq.offer(new int[]{nextNode, newDist});
+        for(int k = 1 ; k <= N ; k++){
+            for(int i = 1 ; i <= N ; i++){
+                for(int j = 1 ; j <= N; j++){
+                    if(map[i][k] != INF && map[k][j] != INF)
+                        map[i][j] = Math.min(map[i][k] + map[k][j], map[i][j]);
                 }
             }
+        }
+
+        for(int i = 1 ; i <= N ; i++){
+            for(int j = 1 ; j <= N; j++){
+                if(map[i][j] == INF || i == j) System.out.print(0 + " ");
+                else
+                    System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
