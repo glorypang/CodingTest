d40058bdeb30152cca15c26ebd3baf71947d3a5b
Glory <78744049+glorypang@users.noreply.github.com>
2026-03-08 23:32:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1064 ms, Memory: 70652 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,46 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static List<List<Integer>> g;
+
+public class Main{
+    static List<List<Integer>> list = new ArrayList<>();
+    static int[] answer;
     static boolean[] visit;
-    static int N;
-    static int[] res;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i<= N ;i++)
+            list.add(new ArrayList<>());
+        visit = new boolean[N+1];
+        answer = new int[N+1];
 
-        N = Integer.parseInt(br.readLine());
-        visit = new boolean[N + 1];
-        res = new int[N + 1];
-        g = new ArrayList<>();
-        for (int i = 0; i < N + 1; i++) {
-            g.add(new ArrayList<>());
-        }
-        for (int i = 0; i < N-1; i++) {
+        for(int i = 0; i < N-1; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            g.get(u).add(v);
-            g.get(v).add(u);
-        }
-        dfs(1);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-        for(int i = 2 ; i<= N ; i++){
-            System.out.println(res[i]);
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+        bfs(1);
+
+        for(int i = 2; i <= N; i++){
+            System.out.println(answer[i]);
         }
     }
+    public static void bfs(int n){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
 
-    public static void dfs(int n) {
-        visit[n] = true;
-        for (int next : g.get(n)) {
-            if (!visit[next]) {
-                visit[next] = true;
-                res[next] = n;
-                dfs(next);
+        while(!q.isEmpty()){
+            int cur = q.poll();
 
+            for(int next : list.get(cur)){
+                if(!visit[next]){
+                    answer[next] = cur;
+                    visit[next] = true;
+                    q.add(next);
+                }
             }
         }
     }
