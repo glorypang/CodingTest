90daf8b9b1b6d1017ca29e04169f0fd5988f76f9
Glory <78744049+glorypang@users.noreply.github.com>
2026-03-10 10:59:02 +0900

    [Gold II] Title: 트리의 지름, Time: 2192 ms, Memory: 112240 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,63 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<List<int[]>> graph;
-    static final int INF = Integer.MAX_VALUE;
-    static boolean[] visited;
-
+    static List<List<int[]>> list = new ArrayList<>();
+    static final int INF = 1_000_000_000;
+    static int[] dist;
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        graph = new ArrayList<>();
-        int n = Integer.parseInt(st.nextToken());
-        for(int i =0 ; i < n+1; i++){
-            graph.add(new ArrayList<>());
+        N = Integer.parseInt(st.nextToken());
+        for(int i =0 ; i <= N ; i++){
+            list.add(new ArrayList<>());
         }
-        visited = new boolean[n+1];
-        for(int i =0  ; i < n; i++){
+
+        for(int i = 0 ; i < N; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
+
             while(true){
                 int e = Integer.parseInt(st.nextToken());
                 if(e == -1) break;
                 int w = Integer.parseInt(st.nextToken());
-                graph.get(s).add(new int[]{e, w});
-                graph.get(e).add(new int[]{s, w});
+
+                list.get(s).add(new int[]{e, w});
             }
         }
-        Node target1 = dfs(1,0);
-        Arrays.fill(visited, false);
 
-        Node target2 = dfs(target1.node, 0);
-        System.out.println(target2.distance);
+        int max = Integer.MIN_VALUE;
+//        for(int i = 1; i <= N; i++){ // root 순회
+            // 가장 깊은 노드 찾고
+            dijkstra(1);
+
+            int depthNode = 0;
+            int length = 0;
+            for(int j = 1; j <= N; j++){
+                if(dist[j] > length){
+                    length = dist[j];
+                    depthNode = j;
+                }
+            }
+            // 해당 노드에서 다익스트라로 가장 먼 노드찾기
+            dijkstra(depthNode);
+
+            for(int j =  1; j <= N; j++){
+                max = Math.max(dist[j], max);
+            }
+//        }
+        System.out.println(max);
     }
-    public static Node dfs(int start, int distance){
-        visited[start] = true;
-        Node node = new Node(start, distance);
+    static void dijkstra(int start){
+        dist = new int[N+1];
+        for(int i = 0; i<= N; i++)
+            Arrays.fill(dist, INF);
 
-        for(int[] next : graph.get(start)){
-            int nextNode = next[0];
-            int weight = next[1];
+        PriorityQueue<int[]> pq= new PriorityQueue<>((o1, o2)-> o1[1] - o2[1]);
+        pq.offer(new int[]{start, 0});
+        dist[start] = 0;
 
-            if(!visited[nextNode]){
-                Node newNode = dfs(nextNode, distance+weight);
-                if(newNode.distance > node.distance){
-                    node = newNode;
+        while (!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int curNode= cur[0];
+            int curDist = cur[1];
+
+            if(curDist != dist[curNode]) continue;
+
+            for(int[] next : list.get(curNode)){
+                int nextNode = next[0];
+                int weight = next[1];
+
+                int newDist = weight + curDist;
+                if(newDist < dist[nextNode]){
+                    dist[nextNode] = newDist;
+                    pq.offer(new int[]{nextNode, newDist});
                 }
             }
         }
-        return node;
-    }
-    public static class Node{
-        int node;
-        int distance;
 
-        public Node(int node, int distance){
-            this.node = node;
-            this.distance = distance;
-        }
     }
 }
