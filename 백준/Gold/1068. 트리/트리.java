8537e5fd682f84dc30dbb27bbd6ae9b94c586644
Glory <78744049+glorypang@users.noreply.github.com>
2026-03-12 10:32:23 +0900

    [Gold V] Title: 트리, Time: 136 ms, Memory: 16264 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,60 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static List<List<Integer>> graph;
-    static int leafCount = 0;
-    static int deleteNode;
-    static boolean[] visited;
-    static int root;
 
+public class Main {
+    public static class Node{
+        int idx;
+        List<Node> child = new ArrayList<>();
+
+        Node(int n){
+            this.idx = n;
+        }
+
+        void add(Node n){
+            child.add(n);
+        }
+        void remove(Node n){
+            child.remove(n);
+        }
+        boolean contain(Node n){
+            return child.contains(n);
+        }
+    }
+    static int answer =0 ;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        graph = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            graph.add(new ArrayList<>());
-        visited = new boolean[n];
-
+        HashMap<Integer, Node> map = new HashMap<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int child = 0; child < n; child++) {
-            int parent = Integer.parseInt(st.nextToken());
+        Node rootNode = null;
 
-            if (parent == -1) {
-                root = child;
-            } else {
-                graph.get(parent).add(child);
-                graph.get(child).add(parent); // 무방향
-            }
+        for (int i = 0; i < N; i++) {
+            map.put(i, new Node(i));
         }
-
-        deleteNode = Integer.parseInt(br.readLine());
-
-        if (deleteNode == root) {
+        for(int i = 0 ; i < N; i++){
+            int num  = Integer.parseInt(st.nextToken());
+            if(num == -1) rootNode = map.get(i);
+            else map.get(num).add(map.get(i));
+        }
+        int K = Integer.parseInt(br.readLine());
+        Node temp = map.get(K);
+        if(temp == rootNode){
             System.out.println(0);
-            return;
+            System.exit(0);
         }
-
-
-
-        dfs(root, -1);
-        System.out.println(leafCount);
-    }
-    static void dfs(int node, int parent) {
-        if(node == deleteNode) return;
-        boolean isLeaf = true;
-
-        for(int next : graph.get(node)) {
-            if(next != parent && next != deleteNode) {
-                isLeaf = false;
-                dfs(next, node);
+        for(int i = 0; i < N; i++){
+            Node node = map.get(i);
+            if(node.contain(temp)){
+                node.remove(temp);
             }
         }
-        if(isLeaf) {
-            leafCount++;
+        dfs(rootNode);
+        System.out.println(answer);
+    }
+
+    public static void dfs(Node node){
+        if(node.child.isEmpty())
+            answer++;
+
+        for(Node next : node.child){
+            dfs(next);
         }
     }
 }
