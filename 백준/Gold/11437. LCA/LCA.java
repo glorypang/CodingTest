7a45501ae382e8d3d0c8f2e726fc34143a2da0bd
Glory <78744049+glorypang@users.noreply.github.com>
2026-03-30 09:43:23 +0900

    [Gold III] Title: LCA, Time: 1500 ms, Memory: 46280 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -2,70 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<List<Integer>> list = new ArrayList<>();
+    static List<List<Integer>> list =new ArrayList<>();
     static int[] parent;
+    static boolean[] visit;
     static int[] depth;
-    static boolean[] visited;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
-        for(int i = 0 ; i <= N ; i++)
+        parent = new int[N+1];
+        visit = new boolean[N+1];
+        depth = new int[N+1];
+        for(int i = 0; i <= N; i++)
             list.add(new ArrayList<>());
 
+        StringTokenizer st;
         for(int i = 1; i < N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st =new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
 
             list.get(u).add(v);
             list.get(v).add(u);
         }
-        
-        parent = new int[N + 1];
-        depth = new int[N + 1];
-        visited = new boolean[N + 1];
-        
+
         dfs(1, 0, 0);
 
         int M = Integer.parseInt(br.readLine());
-        for(int i = 0; i < M; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < M; i++){
+            st =new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
+
             int lca = LCA(u, v);
             System.out.println(lca);
         }
     }
 
-    static void dfs(int node, int par, int d){
-        visited[node] = true;
+    static void dfs(int node, int par, int de){
+        visit[node] = true;
         parent[node] = par;
-        depth[node] = d;
+        depth[node] = de;
 
-        for(int next: list.get(node)){
-            if(!visited[next]){
-                dfs(next, node, d + 1);
+        for(int next : list.get(node)){
+            if(!visit[next]){
+                dfs(next, node, de+1);
             }
         }
     }
-    
-    static int LCA(int a, int b){
-        // 깊이 맞추기
-        while(depth[a] > depth[b]){
-            a = parent[a];
+
+    static int LCA(int node1, int node2){
+        while(depth[node1] > depth[node2])
+            node1 = parent[node1];
+        while(depth[node1] < depth[node2])
+            node2 = parent[node2];
+
+        while(node1 != node2){
+            node1 = parent[node1];
+            node2 = parent[node2];
         }
-        while(depth[b] > depth[a]){
-            b = parent[b];
-        }
-        
-        // 같이 올라가기
-        while(a != b){
-            a = parent[a];
-            b = parent[b];
-        }
-        
-        return a;
+        return node1;
     }
 }
