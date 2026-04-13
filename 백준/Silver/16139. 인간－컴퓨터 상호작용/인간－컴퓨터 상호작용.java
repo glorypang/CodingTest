033aa229a510fc078e1c03bcad93af664c4c0821
Glory <78744049+glorypang@users.noreply.github.com>
2026-04-13 22:17:45 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 172 ms, Memory: 15768 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -7,24 +7,26 @@ public class Main {
         String S = br.readLine();
         int[][] arr = new int[26][S.length()+1];
 
-        for(int i = 1 ; i <= S.length(); i++){
-            arr[S.charAt(i-1)-'a'][i]++;
+        // 각 문자 위치 표시 (1-based로 저장)
+        for(int i = 0; i < S.length(); i++){
+            arr[S.charAt(i)-'a'][i+1]++;
         }
 
-        for(int i = 0 ; i < 26; i++){
-            for(int j = 1; j <= S.length() ; j++){
+        // 누적 합 계산
+        for(int i = 0; i < 26; i++){
+            for(int j = 1; j <= S.length(); j++){
                 arr[i][j] += arr[i][j-1];
             }
         }
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < N; i++){
-            st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             char c = st.nextToken().charAt(0);
-            int n1 = Integer.parseInt(st.nextToken());
-            int n2 = Integer.parseInt(st.nextToken());
-            System.out.println(arr[c-'a'][n2+1]-arr[c-'a'][n1]);
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            // l~r 구간: arr[r+1] - arr[l]
+            System.out.println(arr[c-'a'][r+1] - arr[c-'a'][l]);
         }
     }
 }
