b5cf641315a77b57443470ad9183ba3e94612921
Glory <78744049+glorypang@users.noreply.github.com>
2026-04-20 10:41:20 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.39 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,16 +1,25 @@
 import java.util.*;
+
 class Solution
 {
     public int solution(int []A, int []B)
     {
         int answer = 0;
-        Arrays.sort(A);
-        Arrays.sort(B);
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        
+        for(int a : A)
+            list1.add(a);
+        for(int a : B)
+            list2.add(a);
+        
+        Collections.sort(list1);
+        Collections.sort(list2);
         
         for(int i = 0 ; i < A.length; i++){
-            answer += A[i] * B[B.length-i-1];
+            answer += list1.get(i) * list2.get(list2.size()-i-1);
         }
-
+    
         return answer;
     }
 }
