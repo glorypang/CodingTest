97fc44c1a66bc809091ee2d089c358b2b3a67515
Glory <78744049+glorypang@users.noreply.github.com>
2026-04-21 15:09:56 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.22 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 82.1 MB, 시간: 0.21 ms
+메모리: 73.9 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 01일 12:01:48
+2026년 04월 21일 15:09:54
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,30 +1,21 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = {};
-        int cnt = 1;
-        List<String> list = new ArrayList<>();
-        list.add(words[0]);
-        for(int i =1; i < words.length; i++){
-            if(list.contains(words[i])) {
-                //System.out.println("fail conitans: " + words[i]);
+        List<String> word = new ArrayList<>();
 
-                break;
+        
+        String before ="";
+        for(int i = 0 ; i <words.length; i++){
+            if(word.contains(words[i]) || (i>0 && before.charAt(before.length()-1) != words[i].charAt(0))){
+                return new int[]{i%n+1, i/n+1};
             }
-            String listStr = list.get(list.size()-1);
-            if(listStr.charAt(listStr.length()-1) != words[i].charAt(0)) {
-                            //System.out.println("fail lead: " + words[i]);
-
-                break;
+            else
+                word.add(words[i]);
             
-            }
-            list.add(words[i]);
-            //System.out.println("Insert Str: " + words[i]);
-            cnt++;
+            before = words[i];
+            
         }
-        if(cnt == words.length) return new int[]{0,0};
-        cnt++;
-        return new int[]{cnt%n==0 ? n : cnt%n, cnt%n==0 ?  cnt/n: cnt/n+1};
+        return new int[]{0, 0};
     }
 }
